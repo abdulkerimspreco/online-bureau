230afdf859031260f7a9b623c149a87e66c14d32
abdulkerimspreco <abdul-.kerim.spreco@stu.ibu.edu.ba>
2026-05-03 14:10:00 +0200

    scaffold backend application and prisma foundation

---
+++ apps/backend/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { JobSeekersModule } from './job-seekers/job-seekers.module';
+import { EmployersModule } from './employers/employers.module';
+import { CvModule } from './cv/cv.module';
+import { TagsModule } from './tags/tags.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    PrismaModule,
+    JobSeekersModule,
+    EmployersModule,
+    CvModule,
+    TagsModule,
+  ],
+})
+export class AppModule { }
